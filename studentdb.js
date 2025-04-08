336060f083832350c27952103e809f54a79d7a68
Nuwani Sudarshika <127819371+NuwaniSudarshika@users.noreply.github.com>
2025-04-08 18:57:18 +0530

    Add files via upload

---
+++ studentdb.js
@@ -0,0 +1,8 @@
+let  students = [
+    {regno:"2021ICT01",Name:"Silwa",age:25,gender:"male"},
+    {regno:"2021ICT02",Name:"Nawarathna",age:24,gender:"female"},
+    {regno:"2021ICT03",Name:"Ariyawansha",age:26,gender:"female"},
+    {regno:"2021ICT04",Name:"Wijeykoon",age:25,gender:"male"},
+    {regno:"2021ICT05",Name:"Dayarathna",age:23,gender:"female"}
+];
+module.exports = students;
